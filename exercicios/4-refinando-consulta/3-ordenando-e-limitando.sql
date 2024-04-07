4723ffaaaebe2e41f0700546f7d3a902a7a9e636
Alesandro <108628415+alesandrocordeiro@users.noreply.github.com>
2024-04-07 16:39:26 +0000

    exercice5

--- exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
+++ exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
@@ -1,7 +1,13 @@
--- Desenvolva uma consulta para a tabela "customers" que retorne apenas 10 linhas
+SELECT
+CustomerId AS id,
+FirstName AS nome,
+Country AS pais 
+FROM
+customers
+ORDER BY
+pais ASC,
+nome DESC
+LIMIT 10;
 
 
--- Usando a consulta anterior, ordene a coluna "Country" de forma crescente e "FirstName" de forma decrescente
-
-
--- Continue usando a consulta anterior, dessa vez retorne 10 resultados, no entando ignore as 3 primeiras linhas
+--OFFSET 3;
